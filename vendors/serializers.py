f508e0b9ca687cf2d9a6a3093a86f177525278bb
Paritosh Jadhav <paritosh-developer@outlook.com>
2024-05-05 16:32:41 +0530

    vendors/serializers.py --> created vendor serializer

---
+++ vendors/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Vendor
+
+class VendorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vendor
+        fields = '__all__'
+        read_only_fields = [ 'on_time_delivery_rate', 'quality_rating_avg', 
+                            'average_response_time', 'fulfillment_rate'
+                            ]
