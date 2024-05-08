ba626580f61361cd5652b272560b7bb3ee4f8ecb
Paritosh Jadhav <paritosh-developer@outlook.com>
2024-05-08 20:30:39 +0530

    vendors/serializers --> added vendor_code logic and None values to later 4 fields

--- vendors/serializers.py
+++ vendors/serializers.py
@@ -1,10 +1,26 @@
 from rest_framework import serializers
 from .models import Vendor
+import string
+import random
 
 class VendorSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Vendor
         fields = '__all__'
-        read_only_fields = [ 'on_time_delivery_rate', 'quality_rating_avg', 
-                            'average_response_time', 'fulfillment_rate'
-                            ]
+
+    def create(self, validated_data):
+        ascii = string.ascii_uppercase
+        digits = string.digits
+
+        complete_code = ascii + digits
+
+        validated_data['vendor_code'] = ''.join(random.choices(complete_code, k=5))
+
+        validated_data['on_time_delivery_rate'] = None
+        validated_data['quality_rating_avg'] = None
+        validated_data['average_response_time'] = None
+        validated_data['fulfillment_rate'] = None
+
+        return Vendor.objects.create(**validated_data)
+
