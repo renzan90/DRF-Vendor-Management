8869a2de1a6f38945daa03c311a4b06d8fea9b85
Paritosh Jadhav <paritosh-developer@outlook.com>
2024-05-11 03:16:02 +0530

    orders/serializers.py --> added update func- if status=completed save the date

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
 from .models import PurchaseOrder
+import string
+import random
+from django.utils import timezone
+
 
 class PurchaseOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = 'PurchaseOrder'
         fields = '__all__'
+
+    def create(self, validated_data):
+        digits = string.digits
+
+        validated_data['po_number'] = 'po-'+''.join(random.choices(digits, k=5))
+
+        return PurchaseOrder.objects.create(**validated_data)
+    
+    def update(self, validated_data):
+
+        if validated_data['status'] == 'completed':
+            self.real_delivery_date = timezone.now()
