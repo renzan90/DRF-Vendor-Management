5b724eca5dab8f5c8144518f421d644fdc24834d
Paritosh Jadhav <paritosh-developer@outlook.com>
2024-05-11 21:36:06 +0530

    orders/models.py --> status and rating choices changed with Choices

--- orders/models.py
+++ orders/models.py
@@ -2,28 +2,20 @@ from django.db import models
 from vendors.models import Vendor
 from django.core.exceptions import ValidationError
 from django.utils import timezone
+from model_utils import Choices
 
 class PurchaseOrder(models.Model):
-    STATUS = {
-        "P":"pending",
-        "CO":"completed",
-        "CA":"cancelled",
-    }
+    STATUS = Choices("pending", "completed", "cancelled")
 
-    RATING = {
-        "ONE":"bad",
-        "TWO":"below average",
-        "THREE":"average",
-        "FOUR":"above average",
-        "FIVE":"excellent"
-    }
-    po_number = models.CharField(unique=True)
+    RATING = Choices("bad", "below average", "average", "above average", "excellent")
+    
+    po_number = models.CharField(unique=True, default=None)
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     order_date = models.DateTimeField(auto_now_add=True)
     delivery_date = models.DateTimeField()
     items = models.JSONField()
     quantity = models.IntegerField()
-    status = models.CharField(choices=STATUS, default=STATUS[0])
+    status = models.CharField(choices=STATUS, default=STATUS.pending)
     quality_rating = models.FloatField(choices=RATING, null=True)
     issue_date = models.DateTimeField()
     acknowledgment_date = models.DateTimeField(null=True)
